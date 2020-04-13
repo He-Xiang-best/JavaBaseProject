cb5c62054a0c1f336905786a1b6aefc9e8588843
He-Xiang-best <172837855@qq.com>
2020-04-13 23:59:52 +0800

    实验题

---
+++ src/ex01/Test_7.java
@@ -0,0 +1,16 @@
+package ex01;
+/**
+ * 
+ * @content:思考控制台的输出和键盘输入语句
+ * @author:04191315何翔
+ * @time:2020年4月13日下午11:51:33
+ */
+public class Test_7 {
+
+	public static void main(String[] args) {
+     System.out.println("系统输出语句：System.out.println/print/printf");
+     System.out.println("系统输入语句：Scanner/BufferedInputStream等");
+
+	}
+
+}

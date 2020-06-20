7ea55cb90747731f422cab5eb34895a52a1f68ea
He-Xiang-best <172837855@qq.com>
2020-06-20 10:17:15 +0800

    .

--- Java基础/Java程序设计课程设计/src/A/A03.java
+++ Java基础/Java程序设计课程设计/src/A/A03.java
@@ -5,12 +5,22 @@ import java.util.Scanner;
 /**
  * 请写出一个程序能够将给定的整数转换为若干行星号（“*”）。
  * 具体的转换规则如下：例如整数以十进制表示，如果给定正数第i位是n，则倒数第i行输出n个星号
+ * @author 何翔
  */
 public class A03 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("请输入一串数字");
-        String num = input.next();
+        print(input.next());
+    }
+    public  static void  print(String num){
+        /**
+         * @description:打印信息
+         * @params:[num]
+         * @return: void
+         * @author: 何翔
+         * @time: 2020/6/14 15:55
+         */
         System.out.println(num+" is converted into：");
         for(int i =0;i<num.length();i++){
             for (int j=0;j<Integer.parseInt(String.valueOf(num.charAt(num.length()-(i+1))));j++){
---
+++ Java基础/Java程序设计课程设计/src/A/A05.java
@@ -0,0 +1,30 @@
+package A;
+
+/**
+ *编写程序输出从1到9的乘法口诀表，口诀的前一部分是：
+ * 1*1=1
+ * 1*2=2；2*2=4；
+ * 1*3=3；2*3=6；3*3=9；
+ * ……
+ */
+public class A05 {
+    public static void main(String[] args) {
+        print(9);
+    }
+
+    public static void print(int num){
+/**
+ * @description:
+ * @params:[num]
+ * @return: void
+ * @author: 何翔
+ * @time: 2020/6/14 15:41
+ */
+        for (int i=1;i<=num;i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " * " + i + " = " + (i * j) + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
